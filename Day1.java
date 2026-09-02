62b9afc91171de9916192e4b322619c0725eb044
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-02 21:39:06 +0800

    Day 1

---
+++ Day1.java
@@ -0,0 +1,21 @@
+ // public class adalah struktur utama dalam bahasa Java yang berfungsi sebagai wadah untuk menulis kode
+
+public class Day1 {
+   public static void main(String[] args){
+    
+    
+    // (Comment single line) Jenis komen di java tapi hanya 1 baris saja dan bisa di taro di samping kode
+
+    
+    /*(Comment multi line) 
+    Jenis komen java yang bisa      
+    berbaris baris */
+
+    
+    /** (Comment documentation) Jenis komen java yang sedikit mirip dengan multi line 
+     * digunakan untuk mendokumentasikan kode 
+     * dan bisa diakses oleh tools dokumentasi */
+     
+
+   }
+}
