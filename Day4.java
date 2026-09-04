c62bee80240e414fded485d1d32652d86002f15b
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-05 01:24:43 +0800

    Day4
    Add a Java class with numeric data type examples.

---
+++ Day4.java
@@ -0,0 +1,16 @@
+public class Day4 {//e
+    public static void main (String[] args){ //f
+        // tipe data numerik (efraim)
+        int a = 10000000;            //bisa menampung -2.1 Milliar sampai 2.1 Milliar
+        byte b = 100;                //bisa menampung nilai -127 sampai 127
+        short c = 1000;              //bisa menampung nilai -32 768 sampai 32.767
+        long d = 1000000000L;        //di gunakan untuk angaka yang  sangat besar
+
+        System.out.println("Nilai a = " + a);//r
+        System.out.println("Nilai b = " + b);//a  
+        System.out.println("Nilai c = " + c);//i
+        System.out.println("Nilai d = " + d);//m
+
+    }
+    
+}
