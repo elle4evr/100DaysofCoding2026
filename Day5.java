c1081ab8ee953a4a8af4f297814c4e8399cac530
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-06 00:10:05 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,22 @@
+public class Day5 {
+    /**
+     * @author Efraim
+     */
+    public static void main (String [] args) {
+    // Day 5 tipe data numerik pecahan
+
+    double high = 168.8;
+    float bb = 55.4f;
+
+    System.out.println("======== CONTOH BILANGAN NUMERIK PECAHAN ========");
+    System.out.println("Tinggi badan : " + high);
+    System.out.println("Berat badan  : " + bb);
+
+    System.out.println("");
+
+    System.out.println("==== Nilai MIN dan MAX tipe data numerik bilangan pecahan ====");
+    System.out.println("Contoh bilangan double dari " + Double.MIN_VALUE+ " sampai " + Double.MAX_VALUE );
+    System.out.println("Contoh bilangan float dari " + Float.MIN_VALUE +" sampai " + Float.MAX_VALUE );
+    System.out.println("");
+    }
+}
