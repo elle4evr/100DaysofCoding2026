4ef114b8f7696aeb908426cc8bbd6fa7715c7eef
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-07 01:27:38 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,20 @@
+public class Day6 {
+    /** 
+     * @author Efraim
+     */
+    public static void main(String [] args) {
+    // Day 6: char dan boolean
+    char e = 'r';
+    char f =  'a';
+    boolean i = true;
+    boolean m = false;
+
+    System.out.println("char dipakai untuk menyimpan satu karakter tunggal, seperti huruf, angka, atau simbol");
+    System.out.printf("Contohnya %s dan %s",e,f);
+    
+    
+    System.out.println("\n\nboolean dipakai untuk menyimpan 2 nilai saja yaitu true dan false");
+    System.out.printf("Contohnya %s dan %s",i,m);
+    }
+    
+}
