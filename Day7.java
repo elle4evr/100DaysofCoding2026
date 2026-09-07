1a2b5879a59ff9bd46efe65254d40775922800ca
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-08 00:43:33 +0800

    Add Day7 class with main method and string example

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class Day7 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+    // Day 7 : pengenalan tipe data string, string digunakan untuk menyimpan urutan karakter atau teks
+
+    String jurusan, alamat;
+    jurusan = "informatika";
+    alamat = "Mamasa";
+
+    System.out.printf("Saya mengambil jurusan %s dan alamat saya berada di %s",jurusan,alamat);
+    }
+}
