b89d6cc33690b2f6101530db414618bc2a2ccd5c
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-09 00:09:48 +0800

    Add Day8 Java program for age calculation
    Implement Day 8 Java program with variable declaration, initialization, and update.

---
+++ Day8.java
@@ -0,0 +1,21 @@
+public class Day8 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+        //Day 8 Deklarasi inisialisasi dan update variable
+        
+
+        //Deklarasi
+         int umur;
+
+        //Inisialisasi
+        umur = 17;
+        System.out.printf("Tahun 2025 saya berumur %d tahun",umur);
+
+        //update variable
+        umur = 18;
+        System.out.printf("\ndan pada saat 2026 umur saya sudah %d tahun",umur);
+
+    }
+}
