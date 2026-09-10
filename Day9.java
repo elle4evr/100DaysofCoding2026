118632f2dc0d1d7508f00b92b10b72b534d46263
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-10 10:38:53 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class Day9 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+    // konstanta (final variable)
+    final String I_LUVV = "elle";
+    System.out.printf("Im like %s ",I_LUVV);
+    }
+}
