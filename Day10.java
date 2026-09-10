3ae8a9b530a015c10780f403165139cdf0058ee7
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-11 01:03:39 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+public class Day10 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+        // batas tipe data
+    
+    System.out.println("Batas minimal tipe data byte adalah "+ Byte.MIN_VALUE + " Sampai dengan " + Byte.MAX_VALUE );
+    System.out.println("Batas minimal tipe data short adalah "+ Short.MIN_VALUE + " Sampai dengan " + Short.MAX_VALUE );
+    System.out.println("Batas minimal tipe data int adalah "+ Integer.MIN_VALUE + " Sampai dengan " + Integer.MAX_VALUE );
+    System.out.println("Batas minimal tipe data long adalah "+ Long.MIN_VALUE + " Sampai dengan " + Long.MAX_VALUE );
+
+    // tipe data desimal
+    System.out.println("Batas minimal tipe data float adalah " + Float.MIN_VALUE + " Sampai dengan " + Float.MAX_VALUE);
+    System.out.println("Batas minimal tipe data double adalah "+ Double.MIN_VALUE + " Sampai dengan " + Double.MAX_VALUE );
+    }
+    
+}
