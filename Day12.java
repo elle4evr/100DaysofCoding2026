bcb04bb3048e3f119fe3223a6b39a3562bf006c5
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-13 14:25:19 +0800

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -4,36 +4,41 @@ public class Day12 {
      * @author Efraim
      */
     public static void main(String[] args) {
+        // membuat bidata menggunakan scanner
     
-    String nama, nim, alamat, jurusan;
+    String nama, jk, alamat, jurusan, hobi;
     int umur;
 
-    System.out.println("::......BIODATA......::");
     Scanner data = new Scanner(System.in);
 
-    System.out.print("Masukkan Nama\t: ");
+    System.out.print("Masukkan Nama\t\t: ");
     nama = data.nextLine();
 
-    System.out.print("Masukkan Umur\t: ");
+    System.out.print("Masukkan Umur\t\t: ");
     umur = data.nextInt();
     data.nextLine(); 
 
-    System.out.print("Masukkan NIM\t: ");
-    nim = data.nextLine();
+    System.out.print("Masukkan Jenis Kelamin\t: ");
+    jk = data.nextLine();
 
-    System.out.print("Masukkan Alamat\t: ");
+    System.out.print("Masukkan Alamat\t\t: ");
     alamat = data.nextLine();
 
-    System.out.print("Masukkan Jurusan: ");
+    System.out.print("Masukkan Jurusan\t: ");
     jurusan = data.nextLine();
+    
+    System.out.print("Masukkan Hobi\t\t: ");
+    hobi = data.nextLine();
 
 
-    System.out.println("\n::......BIODATA......::");
-    System.out.println("Nama saya\t: "+nama);
-    System.out.println("Umur saya\t: " + umur +" tahun");
-    System.out.println("NIM saya\t: "+nim);
-    System.out.println("Alamat saya\t: "+alamat);
-    System.out.println("Jurusan saya\t: "+jurusan);
-    System.out.println("");
+    System.out.println("\n===== BIODATA =====");
+    System.out.println("Nama\t\t: "+nama);
+    System.out.println("Umur\t\t: " + umur +" tahun");
+    System.out.println("Jenis Kelamin\t: "+jk);
+    System.out.println("Alamat\t\t: "+alamat);
+    System.out.println("Jurusan\t\t: "+jurusan);
+    System.out.println("Hobi\t\t: "+hobi);
+    System.out.println("===================");
     }
     
+}
