ca5fcd723042e1231d9398e9844fb79d2674cb30
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-15 20:28:55 +0800

    Implement basic arithmetic operations in Day14

---
+++ Day14.java
@@ -0,0 +1,19 @@
+public class Day14 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+    int a,b;
+    a = 15;
+    b = 5;
+    
+    //Penjumlahan
+    a += b;
+    System.out.println("Hasil Pemjumlahan : " + a);
+    
+    //Pengurangan
+    a -= b;
+    System.out.println("Hasil Pengurangan : " + a);
+
+    }
+}
