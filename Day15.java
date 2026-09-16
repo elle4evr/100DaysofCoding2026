5284c5d9f7fdb8405b00c0aacf23c3a378cb92a3
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-16 23:30:10 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,17 @@
+public class Day15 {
+    /**
+     * @author
+     */
+    public static void main(String[] args) {
+    
+    int a,b, h1, h2;
+    a = 16;
+    b = 8;
+
+    h1 = a*b;
+    h2 = a/b;
+
+    System.out.printf("Hasil perkalian dari %d * %d = %d%n", a,b ,h1 );
+    System.out.printf("Hasil pembagian dari %d / %d = %d%n", a,b ,h2 );
+    }
+}
