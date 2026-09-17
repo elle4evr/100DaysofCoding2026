6143ca40ea0babe067fc98a7264c8b3efcdea6ac
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-17 23:40:31 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,9 @@
+public class Day16 {
+    public static void main(String[] args) {
+    int a = 9;
+    int b = 10;
+    
+    int h = a%b;
+    System.out.printf("Hasil dari modulus %d %% %d : %d%n", a,b, h);
+    }
+}
