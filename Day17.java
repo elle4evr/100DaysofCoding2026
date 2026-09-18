0f03f4de9aa0d18f0b7cece9d73613954a0e2038
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-18 20:55:52 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,24 @@
+public class Day17 {
+    /**
+     * @author Efraim
+     */
+    public static void main(String[] args) {
+        // Day 17 operator penugasan
+        int angka =2;
+
+        angka += 1; 
+        System.out.println(angka);
+
+        angka -= 2; 
+        System.out.println(angka);
+
+        angka *= 2;
+        System.out.println(angka);
+
+        angka /= 2;
+        System.out.println(angka);
+
+        angka %= 1;
+        System.out.println(angka);
+    }
+}
