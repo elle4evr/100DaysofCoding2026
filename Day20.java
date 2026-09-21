1caf18821b97182428fac727df8ab6648ee80fc1
𝚎𝚕𝚕𝚎'𝚜 / <efraimkamoda80@gmail.com>
2026-09-21 23:45:47 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,14 @@
+public class Day20 {
+    /**
+     * @author Efraim 
+     */
+    public static void main(String[] args) {
+        // mengubah tipe data primitif menjadi tipe data string
+        
+        int e = 18;
+
+        String m = String.valueOf(e);
+
+        System.out.println("Ini int ke string : " + m);
+    }
+}
